0ed4868e90f14f66fdab6c25240ac3107daa4a18
jstevenson1337 <jmstevenson918@gmail.com>
2023-06-08 09:52:58 -0500

    create folder structure

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -8,32 +8,32 @@
  */
 int ec_save(EC_KEY *key, char const *folder)
 {
-	FILE *fp;
-	char path[128] = {0};
+        FILE *fp;
+        char path[128] = {0};
 
-	if (!key || !folder)
-		return (0);
-	mkdir(folder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        if (!key || !folder)
+                return (0);
+        mkdir(folder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
-	sprintf(path, "%s/" PRI_FILENAME, folder);
-	fp = fopen(path, "w");
-	if (!fp)
-		return (0);
-	if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
-	{
-	}
-	fclose(fp);
+        sprintf(path, "%s/" PRI_FILENAME, folder);
+        fp = fopen(path, "w");
+        if (!fp)
+                return (0);
+        if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
+        {
+        }
+        fclose(fp);
 
-	sprintf(path, "%s/" PUB_FILENAME, folder);
-	fp = fopen(path, "w");
-	if (!fp)
-		return (0);
-	if (!PEM_write_EC_PUBKEY(fp, key))
-	{
-		fclose(fp);
-		return (0);
-	}
-	fclose(fp);
+        sprintf(path, "%s/" PUB_FILENAME, folder);
+        fp = fopen(path, "w");
+        if (!fp)
+                return (0);
+        if (!PEM_write_EC_PUBKEY(fp, key))
+        {
+                fclose(fp);
+                return (0);
+        }
+        fclose(fp);
 
-	return (1);
+        return (1);
 }
