09b9425c2be70d93002e872a365dce2ca8517d6c
jstevenson1337 <jmstevenson918@gmail.com>
2023-06-12 18:17:35 -0500

    fir task 0

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -28,7 +28,7 @@ blockchain_t *blockchain_create(void)
 	nb->info.timestamp = 1537578000;
 	memcpy(&(nb->data.buffer), "Holberton School", 16);
 	nb->data.len = 16;
-	memcpy(&(nb->hash), BHASH, SHA256_DIGEST_LENGTH);
+	memcpy(&(nb->hash), "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03", SHA256_DIGEST_LENGTH);
 
 
 	if (llist_add_node(blist, nb, ADD_NODE_FRONT))
