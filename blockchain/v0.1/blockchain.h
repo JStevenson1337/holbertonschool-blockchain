74b3cb3567de97e5984e94609f6873ed09faca17
jstevenson1337 <jmstevenson918@gmail.com>
2023-06-12 18:14:34 -0500

    test task 0

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -1,14 +1,42 @@
 #ifndef __BLOCKCHAIN_H__
-#define __BLOCKCHAIN_H__
+#define __BLOCKHAIN_H__
+
+#include "../../crypto/hblk_crypto.h"
+#include "provided/endianness.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <llist.h>
+#include <time.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <openssl/sha.h>
+
+
+#define BHASH = "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" /* c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803 */
 
 #define BLOCKCHAIN_DATA_MAX 1024
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-#include <stdint.h>
-#include <llist.h>
+/**
+ * struct block_data_s - Block data
+ *
+ * @buffer: Data buffer
+ * @len:    Data size (in bytes)
+ */
+typedef struct block_data_s
+{
+    /*
+     * @buffer must stay first, so we can directly use the structure as
+     * an array of char
+     */
+    int8_t      buffer[BLOCKCHAIN_DATA_MAX];
+    uint32_t    len;
+} block_data_t;
 
 /**
  * struct blockchain_s - Blockchain structure
@@ -19,9 +47,6 @@ typedef struct blockchain_s
 {
     llist_t     *chain;
 } blockchain_t;
-
-
-
 
 /**
  * struct block_info_s - Block info structure
@@ -48,24 +73,6 @@ typedef struct block_info_s
     uint8_t     prev_hash[SHA256_DIGEST_LENGTH];
 } block_info_t;
 
-
-
-/**
- * struct block_data_s - Block data
- *
- * @buffer: Data buffer
- * @len:    Data size (in bytes)
- */
-typedef struct block_data_s
-{
-    /*
-     * @buffer must stay first, so we can directly use the structure as
-     * an array of char
-     */
-    int8_t      buffer[BLOCKCHAIN_DATA_MAX];
-    uint32_t    len;
-} block_data_t;
-
 /**
  * struct block_s - Block structure
  *
@@ -80,6 +87,26 @@ typedef struct block_s
     uint8_t     hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
+/* GENESIS BLOCK - first block in the chain */
+#define GENESIS_BLOCK { \
+        { /* info */ \
+                0 /* index */, \
+                0, /* difficulty */ \
+                1537578000, /* timestamp */ \
+                0, /* nonce */ \
+                {0} /* prev_hash */ \
+        }, \
+        { /* data */ \
+                "Holberton School", /* buffer */ \
+                16 /* len */ \
+        }, \
+        "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+        "\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
+}
+
+
+
 blockchain_t *blockchain_create(void);
+
 
 #endif /* __BLOCKCHAIN_H__ */
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,24 +1,42 @@
 #include "blockchain.h"
+#include <llist.h>
+#include <openssl/sha.h>
+#include <string.h>
+
 /**
- *
- *
- *
- */
+* blockchain_create - creates a blockchain
+*
+* Return: pointer to Blochain || NULL
+*/
 blockchain_t *blockchain_create(void)
 {
-	block_t *nb;
-	blockchain_t *nc;
+	blockchain_t *nbc = NULL;
+	block_t *nb = NULL;
+	llist_t *blist = NULL;
 
+	nbc = calloc(1, sizeof(*nbc));
+	nb = calloc(1, sizeof(*nb));
+	blist = llist_create(MT_SUPPORT_TRUE);
 
-	nb = malloc(sizeof(block_t));
-	if (nb == NULL)
+	if (nbc == NULL || nb == NULL || blist == NULL)
 	{
+		free(nbc);
 		free(nb);
+		free(blist);
 		return NULL;
 	}
-	
+	nb->info.timestamp = 1537578000;
+	memcpy(&(nb->data.buffer), "Holberton School", 16);
+	nb->data.len = 16;
+	memcpy(&(nb->hash), BHASH, SHA256_DIGEST_LENGTH);
 
 
-
+	if (llist_add_node(blist, nb, ADD_NODE_FRONT))
+	{
+		free(nbc), free(nb), llist_destroy(blist, 1, NULL);
+		return (NULL);
+	}
+	nbc->chain = blist;
+	return (nbc);
 
 }
