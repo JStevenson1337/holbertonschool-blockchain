feaa1d8999b0884fb10573f05d6982e6eda2123b
jstevenson1337 <jmstevenson918@gmail.com>
2023-06-12 18:49:42 -0500

    check task 1

---
+++ blockchain/v0.1/block_create.c
@@ -0,0 +1,30 @@
+#include "blockchain.h"
+
+/**
+* block_create - Creates a new block on the chain
+*
+* @prev: pointer to the previous block
+* @data: pointer to data to copy
+* @data_len: lenght of data not exceeding BLOCKCHAIN_DATA_MAX
+*
+* Return: pointer to the block || NULL
+*/
+block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
+{
+	block_t *nb = NULL;
+
+	nb = calloc(1, sizeof(*nb));
+
+	if (nb == NULL)
+	{
+		return(NULL);
+	}
+
+	memcpy(&(nb->data.buffer), data,
+	BLOCKCHAIN_DATA_MIN(data_len,
+	BLOCKCHAIN_DATA_MAX));
+	nb->data.len = BLOCKCHAIN_DATA_MIN(data_len, BLOCKCHAIN_DATA_MAX);
+	nb->info.timestamp = time(NULL);
+	memcpy(&(nb->info.prev_hash), prev->hash, SHA256_DIGEST_LENGTH);
+	return(nb);
+}
