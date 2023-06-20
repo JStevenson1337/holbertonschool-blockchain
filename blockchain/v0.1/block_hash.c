a471e99dd0526f56ad8a031c2bfa33096c33d398
jmstevenson1337 <jmstevenson918@gmail.com>
2023-06-20 20:57:29 +0000

    rebuilt library

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,13 +1,11 @@
 #include "blockchain.h"
-
 /**
  * block_hash - computes the hash of a Block
  * @block: pointer to block to be hashed
  * @hash_buf: where hash will be stored
  * Return: pointer to hash_buf
  **/
-uint8_t
-*block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	size_t len;
 
