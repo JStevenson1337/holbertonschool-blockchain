255a73c65bf308ea855382238c4f931807992a2d
jmstevenson1337 <jmstevenson918@gmail.com>
2023-06-28 10:49:00 +0000

    add header and tests

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,3 +1,4 @@
+#include "hqq
 /**
  *
  * hash_matches_difficulty - Check if a hash matches the mining difficulty
@@ -10,27 +11,31 @@
 
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty)
 {
-	uint32_t i, bit, bits;
+        uint32_t i, bit, bits;
 
-	for (i = 0; i < difficulty / 8; i++)
-	{
-		if (hash[i] != 0)
-			return (0);
-	}
+        for (i = 0; i < difficulty / 8; i++)
+        {
+                if (hash[i] != 0)
+                        return (0);
+        }
 
-	if (difficulty % 8 == 0)
-		return (1);
+        if (difficulty % 8 == 0)
+                return (1);
 
-	bit = 128;
-	bits = difficulty % 8;
+        bit = 128;
+        bits = difficulty % 8;
 
-	while (bits--)
-	{
-		if (!(hash[i] & bit))
-			return (0);
+        while (bits--)
+        {
+                if (!(hash[i] & bit))
+                        return (0);
 
-		bit >>= 1;
-	}
+                bit >>= 1;
+        }
 
+<<<<<<< HEAD
 	return (1);
+=======
+        return (1);
+>>>>>>> c92208d (Add: Test files)
 }
---
+++ blockchain/v0.2/test/main_1.c
@@ -0,0 +1,37 @@
+/* Description: Valid Block */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "blockchain.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	blockchain_t *blockchain;
+	block_t *first, *block;
+
+	blockchain = blockchain_create();
+	first = llist_get_head(blockchain->chain);
+
+	block = block_create(first, (int8_t *)"Holberton", 9);
+	block->info.timestamp = 972;
+	block_hash(block, block->hash);
+	llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+
+	if (block_is_valid(block, first) != 0)
+	{
+		printf("Block invalid\n");
+		blockchain_destroy(blockchain);
+		return (EXIT_FAILURE);
+	}
+	printf("Block is valid\n");
+
+	blockchain_destroy(blockchain);
+
+	return (EXIT_SUCCESS);
+}
