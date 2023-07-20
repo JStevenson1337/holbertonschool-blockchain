1fa4aa8065845fc29cc9ef07a5e937e2d6f857c5
jeremy stevenson <jmstevenson918@gmail.com>
2023-07-20 17:05:42 -0500

    add block mine

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -135,4 +135,8 @@ int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t
 difficulty);
 int block_is_valid(block_t const *block, block_t const *prev_block);
 int is_genesis(block_t const *block);
+
+void block_mine(block_t *block);
+
+
 #endif /* __BLOCKCHAIN_H__ */
