6b513fc408f374ea0ece88d5a27776846dfe610b
jeremy stevenson <jmstevenson918@gmail.com>
2023-07-20 18:36:42 -0500

    update

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -1,6 +1,9 @@
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
 
+#include "../../crypto/hblk_crypto.h"
+#include "provided/endianness.h"
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,30 +18,21 @@
 
 #include <openssl/sha.h>
 
-#include "../../crypto/hblk_crypto.h"
-#include "provided/endianness.h"
-#include "transaction/transaction.h"
-
-
-
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 #define HBLK_MAGIC "HBLK"
-#define HBLK_VERSION "0.3"
-
-#define COINBASE_AMOUNT 50
+#define HBLK_VERSION "0.2"
 
 /**
  * struct blockchain_s - Blockchain structure
  *
- * @chain:	Linked list of Blocks
- * @unspent: Linked list of unspent transaction outputs
+ * @chain: Linked list of pointers to block_t
  */
 typedef struct blockchain_s
 {
-	llist_t	*chain;
-	llist_t	*unspent;
+	llist_t	 *chain;
 } blockchain_t;
+
 
 /**
  * struct block_info_s - Block info structure
@@ -90,14 +84,12 @@ typedef struct block_data_s
  *
  * @info: Block info
  * @data: Block data
- * @transactions: linked list of transaction ids
  * @hash: 256-bit digest of the Block, to ensure authenticity
  */
 typedef struct block_s
 {
 	block_info_t	info; /* This must stay first */
 	block_data_t	data; /* This must stay second */
-	llist_t *transactions;
 	uint8_t	 hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
@@ -114,7 +106,6 @@ typedef struct block_s
 		"Holberton School", /* buffer */ \
 		16 /* len */ \
 	}, \
-	NULL, /* transactions */ \
 	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
 	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
 }
@@ -129,8 +120,8 @@ uint8_t *block_hash(block_t const *block,
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
 llist_t *deserialize_blocks(int fd, uint32_t size, uint8_t endianness);
-int block_is_valid(block_t const *block, block_t const *prev_block,
-	llist_t *all_unspent);
+int block_is_valid(block_t const *block, block_t const *prev_block);
+
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 	uint32_t difficulty);
 void block_mine(block_t *block);
